c637f5e7b444da3e512e385557c9325b402a2f47
CoryMci <corymciver@live.ca>
2022-10-01 14:50:03 -0700

    Inital deployment

--- timer.bundle.js
+++ timer.bundle.js
@@ -11,11 +11,10 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (__WEBPACK_DEFAULT_EXPORT__)
 /* harmony export */ });
-const timer = ((duration) => {
+const timer = (duration) => {
   let time = duration;
   console.log(time);
-}
-)();
+};
 
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (timer);
 
@@ -27,4 +26,4 @@ const timer = ((duration) => {
 /******/ var __webpack_exports__ = (__webpack_exec__("./src/timer.js"));
 /******/ }
 ]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGltZXIuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBOztBQUVBLGlFQUFlLEtBQUsiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly93ZWJwYWNrLWRlbW8vLi9zcmMvdGltZXIuanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc3QgdGltZXIgPSAoKGR1cmF0aW9uKSA9PiB7XG4gIGxldCB0aW1lID0gZHVyYXRpb247XG4gIGNvbnNvbGUubG9nKHRpbWUpO1xufVxuKSgpO1xuXG5leHBvcnQgZGVmYXVsdCB0aW1lcjsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoidGltZXIuYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7QUFBQTtBQUNBO0FBQ0E7QUFDQTs7QUFFQSxpRUFBZSxLQUFLIiwic291cmNlcyI6WyJ3ZWJwYWNrOi8vd2VicGFjay1kZW1vLy4vc3JjL3RpbWVyLmpzIl0sInNvdXJjZXNDb250ZW50IjpbImNvbnN0IHRpbWVyID0gKGR1cmF0aW9uKSA9PiB7XG4gIGxldCB0aW1lID0gZHVyYXRpb247XG4gIGNvbnNvbGUubG9nKHRpbWUpO1xufTtcblxuZXhwb3J0IGRlZmF1bHQgdGltZXI7Il0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
